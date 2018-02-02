7340f608012d0241502704c1fc815f12223ecf72
sam <shamanthakamani.kyasaram@gmail.com>
2018-02-02 22:45:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sam/trial/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sam.trial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.PublicKey;
+
+public class MainActivity extends AppCompatActivity {
+       public Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
